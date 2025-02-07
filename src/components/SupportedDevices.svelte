2f662082fb4c80a52ec049b684083180c5f29a11
diablo <progzone122@gmail.com>
2025-02-07 13:42:44 +0200

    remove target=_blank

--- src/components/SupportedDevices.svelte
+++ src/components/SupportedDevices.svelte
@@ -8,7 +8,7 @@
 
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4 py-10 px-10 lg:px-28">
         {#each devices as device}
-            <a href="{device.link}" target="_blank">
+            <a href="{device.link}">
                 <div class="border-solid border-white hover:border-color9 border-2 p-4 bg-transparent hover:bg-color9 transition transform active:scale-95">
                     <h4>{device.codename ?? "error codename"}</h4>
                     <p>{device.model ?? "error model"}</p>
